d93d44ee068db5bf51e11620329f755c1798ff70
abswowgo-create <abswowgo@gmail.com>
2026-04-06 23:44:16 +0300

    Create task4.sql

---
+++ task4.sql
@@ -0,0 +1,14 @@
+-- Task 4
+
+SELECT 
+    c.name,
+    c.class,
+    AVG(r.position) AS avg_position,
+    COUNT(*) AS race_count,
+    cl.country
+FROM Results r
+JOIN Cars c ON r.car = c.name
+JOIN Classes cl ON c.class = cl.class
+GROUP BY c.name, c.class, cl.country
+ORDER BY avg_position ASC, c.name ASC
+LIMIT 1;
