1ed1883c2181fd64ac4a7b48d81ac60b8b0042ff
abswowgo-create <abswowgo@gmail.com>
2026-04-06 23:40:21 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,40 +1,9 @@
 -- Task 1
 
-WITH RECURSIVE subordinates AS (
-    SELECT 
-        e.EmployeeID,
-        e.EmployeeName,
-        e.ManagerID,
-        e.DepartmentID,
-        e.RoleID
-    FROM Employees e
-    WHERE e.EmployeeID = 1
-
-    UNION ALL
-
-    SELECT 
-        e.EmployeeID,
-        e.EmployeeName,
-        e.ManagerID,
-        e.DepartmentID,
-        e.RoleID
-    FROM Employees e
-    JOIN subordinates s ON e.ManagerID = s.EmployeeID
-)
-
-SELECT 
-    s.EmployeeID,
-    s.EmployeeName,
-    s.ManagerID,
-    d.DepartmentName,
-    r.RoleName,
-    STRING_AGG(DISTINCT p.ProjectName, ', ') AS Projects,
-    STRING_AGG(DISTINCT t.TaskName, ', ') AS Tasks
-FROM subordinates s
-LEFT JOIN Departments d ON s.DepartmentID = d.DepartmentID
-LEFT JOIN Roles r ON s.RoleID = r.RoleID
-LEFT JOIN EmployeeProjects ep ON s.EmployeeID = ep.EmployeeID
-LEFT JOIN Projects p ON ep.ProjectID = p.ProjectID
-LEFT JOIN Tasks t ON s.EmployeeID = t.EmployeeID
-GROUP BY s.EmployeeID, s.EmployeeName, s.ManagerID, d.DepartmentName, r.RoleName
-ORDER BY s.EmployeeName;
+SELECT v.maker, m.model
+FROM Motorcycle m
+JOIN Vehicle v ON m.model = v.model
+WHERE m.horsepower > 150
+  AND m.price < 20000
+  AND m.type = 'Sport'
+ORDER BY m.horsepower DESC;
