61d49faf46edbc8659fb6373674ae6e1f072f8da
abswowgo-create <abswowgo@gmail.com>
2026-04-06 23:49:06 +0300

    Create task6.sql

---
+++ task6.sql
@@ -0,0 +1,10 @@
+-- Task 6
+
+WITH car_stats AS (
+    SELECT c.name, c.class, AVG(r.position) AS avg_pos
+    FROM Cars c
+    JOIN Results r ON c.name = r.car
+    GROUP BY c.name, c.class
+)
+SELECT *
+FROM car_stats;
